a2f1c4a5ea2e34844a2788ecc7c9acfd06000118
Frendi Triarista <frendi@thriveagency.id>
2022-09-11 10:33:00 +0700

    default route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('https://frendi-realtime-chat.netlify.app/');
+    return redirect('https://chat.legotech.id/');
 });
